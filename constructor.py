4592070b72266c12f2213a2fc5011430eeaf26ab
Anuj Kumar <67889993+anuj8052@users.noreply.github.com>
2024-07-23 00:55:04 +0530

    update

--- constructor.py
+++ constructor.py
@@ -1,5 +1,17 @@
+# freecodecanmp
+# https://www.youtube.com/watch?v=Ej_02ICOIgs&ab_channel=freeCodeCamp.org
 class Item:
-    def __init__(self, name, price, quantity):
+    def __init__(self, name: str, price: float, quantity = 0): #quantity default as 0
+
+        # suppose you naver want to recieve negative number of price as well as quantity
+        # there is a method for this also
+        # run validations to the recieved arguments
+
+        assert price >= 0, f"price {price} is not greater or equal to zero"
+        assert quantity>=0, f"Quantity {quantity} is not greater or equal to zero"
+
+        
+        # assign to self self object
         # print(f"An instance is created: {name}")
         # I can assign dynamic atributes in this __init__ method to reduce the hardcoded code or we can say complexity
         # we also call __init__ method as a constructor
@@ -7,8 +19,11 @@ class Item:
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self, x, y):
-        return x*y
+        
+
+
+    def calculate_total_price(self):
+        return self.price*self.quantity
     
 
 item1 = Item("phone", 500, 5)
@@ -22,6 +37,7 @@ item2 = Item("Laptop", 1000, 3)
 # item2.price = 1000
 # item2.quantity = 3
 
+"""
 print(item1.name)
 print(item1.price)
 print(item1.quantity)
@@ -30,4 +46,14 @@ print(item2.name)
 print(item2.price)
 print(item2.quantity)
 print(item2.price*item2.quantity)
+"""
+
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
+
+
+
+
+
+
 
